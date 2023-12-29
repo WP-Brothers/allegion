112e869956857860f4b7e41ded8d7caeb7139267
Tim van Andel <timvanandel@Tims-MacBook-Pro.local>
2023-12-29 12:06:47 +0100

    added atomic field group prototype

---
+++ wp-content/themes/socialbrothers/library/acf/fields/fieldgroups.php
@@ -0,0 +1,18 @@
+<?php 
+
+function ContentFieldGroup ($prefix, $tabExpand = 1) {
+
+  
+
+    $fields = array_merge(getTabFields($prefix, $tabExpand, 'content', __('Content', '_SBB')), getTitleFields($prefix), getSubTitleFields($prefix), getContentFields($prefix));
+
+    return $fields;
+}
+
+function imageVideoFieldGroup ($prefix, $tabExpand = 0) {
+
+    $fields = array_merge(getTabFields($prefix, $tabExpand, 'image_video', __('Afbeelding/Video', '_SBB')), getImageVideoSwitchFields($prefix), getImageFields($prefix), getVideoFields($prefix));
+
+    return $fields;
+}
+
