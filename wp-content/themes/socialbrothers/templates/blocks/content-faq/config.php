36d06d431c4b305f6c38c076655dc7f7c4b593f8
Tim van Andel <timvanandel@Tims-MacBook-Pro.local>
2024-01-03 11:14:50 +0100

    icluded the faq fields

--- wp-content/themes/socialbrothers/templates/blocks/content-faq/config.php
+++ wp-content/themes/socialbrothers/templates/blocks/content-faq/config.php
@@ -2,7 +2,7 @@
 
 $prefix     = 'block_content-faq';
 
-$fields = array_merge(ContentFieldGroup($prefix), faqFieldGroup($prefix));
+$fields = array_merge(ContentFieldGroup($prefix, 1 , 1, 1), faqFieldGroup($prefix));
 
 $block_data = [
     'key'    => $prefix,
