e6a1cc022ea760a6b5720cdc55a76d4b0a3f7c6e
Tim van Andel <timvanandel@tims-mbp.home>
2024-01-04 14:41:17 +0100

    added close all when u open 2 dropdowns

--- wp-content/themes/socialbrothers/src/scripts/mainMenu.ts
+++ wp-content/themes/socialbrothers/src/scripts/mainMenu.ts
@@ -1,19 +1,25 @@
+
 const mainMenu = (menu: HTMLElement) => {
   const dropdowns = menu.querySelectorAll('.menu__item--has-children');
+
+  const closeAll = () => {
+    dropdowns.forEach((dropdown) => {
+      dropdown.classList.remove('show');
+    });
+  }
 
   dropdowns.forEach((dropdown) => {
     const link = dropdown.querySelector('a');
 
     link?.addEventListener('click', (e) => {
       e.preventDefault();
+      closeAll()
       dropdown.classList.toggle('show');
     });
   });
 
   document.addEventListener('scroll', () => {
-    dropdowns.forEach((dropdown) => {
-      dropdown.classList.remove('show');
-    });
+    closeAll()
   });
 };
 
