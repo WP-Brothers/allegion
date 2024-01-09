e8df68b7099daab892bb7367f3cc511559a02b48
Tim van Andel <timvanandel@Tims-MacBook-Pro.local>
2024-01-09 16:14:02 +0100

    added single-product php

--- wp-content/themes/socialbrothers/single-product.php
+++ wp-content/themes/socialbrothers/single-product.php
@@ -10,10 +10,12 @@ Twig::render(
         'index_context',
         [
             'hero' => [
-                'title'              => get_the_title(get_the_ID()),
-                'safety_index'       => get_field("safety_index", get_the_ID()) ?? '',
-                'product_image'       => get_field("safety_index", get_the_ID()) ?? '',
-                'article_number'       => get_field("article_number", get_the_ID()) ?? '',
+                'title'             => get_the_title(get_the_ID()),
+                'safety_index'      => get_field("safety_index", get_the_ID()) ?? '',
+                'product_image'     => get_post_thumbnail_id(get_the_ID()) ?? '',
+                'article_number'    => get_field("article_number", get_the_ID()) ?? '',
+                'price'             => wpb_build_price(get_field("price", get_the_ID())) ?? '',
+                'keurmerken'        => wpb_build_keurmerken(get_the_ID(), 1),
             ],
         ]
     )
