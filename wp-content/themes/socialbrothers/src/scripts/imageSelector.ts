084619f9775b81d7dfc41b9f68153b94f27a224c
Tim van Andel <timvanandel@Tims-MacBook-Pro.local>
2024-01-15 17:22:46 +0100

    added imageselector script

---
+++ wp-content/themes/socialbrothers/src/scripts/imageSelector.ts
@@ -0,0 +1,66 @@
+const imageSelectorInit = (imageSelector: HTMLElement) => {
+    const arrow = document.getElementById('image-selector__arrow');
+    const highlightedImages = imageSelector.querySelectorAll('#image-selector__highlighted-images .image-selector__highlighted-image')
+    
+    const removeActive = () => {
+        const activeImage = imageSelector.querySelector('#image-selector__highlighted-images .image-selector__highlighted-image.active');
+        if(activeImage) {
+            activeImage.classList.remove('active');
+        }
+    }
+
+    const moveArrow = () => {
+        const activeImage = imageSelector.querySelector('#image-selector__highlighted-images .image-selector__highlighted-image.active');
+
+        if(activeImage && arrow) {
+            const wrapperPosition = imageSelector.getBoundingClientRect().left;
+            const activeImagePosition = activeImage.getBoundingClientRect().left;
+            const newArrowLocation = activeImagePosition - wrapperPosition
+            arrow.style.left = newArrowLocation + 'px';
+        }   
+    }
+
+    const changeActiveDisplayImage = () => {
+        const displayImage:HTMLImageElement = document.querySelector('.product-single__display-image')!;
+        const activeImage:HTMLImageElement = imageSelector.querySelector('#image-selector__highlighted-images .image-selector__highlighted-image.active img')!;
+        const activeSrc = activeImage.src;
+
+        displayImage.src = activeSrc;
+        displayImage.srcset = activeImage.srcset;
+    }
+
+    highlightedImages[0].classList.add('active');
+
+    highlightedImages.forEach((image) => {
+        image.addEventListener('click', () => {
+            removeActive();
+            image.classList.add('active');
+            moveArrow();
+            changeActiveDisplayImage();
+        })
+    });
+
+
+
+
+    const modal:HTMLDialogElement = document.querySelector('[data-modal="modal-swiper"]')!
+    const open = imageSelector.querySelector('[data-modal-open="modal-open"]')!
+    const close = modal.querySelector('.modal__close')!
+
+    console.log(imageSelector.querySelector('[data-modal="modal-swiper"]'));
+    console.log(open)
+    console.log(close)
+
+    if(open) {
+
+        open.addEventListener('click', () => {
+            modal.showModal();
+        })
+        
+        close.addEventListener('click', () => {
+            modal.close();
+        })
+    }
+}
+
+export default imageSelectorInit;
