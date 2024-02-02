3ba3b4acad16db5dabd878f1c1b4224789061555
Tim van Andel <timvanandel@tims-mbp.home>
2024-02-02 15:23:52 +0100

    added support for woocommerce

---
+++ wp-content/themes/socialbrothers/library/theme/woocommerce.php
@@ -0,0 +1,10 @@
+<?php
+
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+   add_theme_support( 'woocommerce' );
+}     
+
+if (class_exists('Woocommerce')){
+    add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+}
